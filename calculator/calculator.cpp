c005182211d84fed6958aa289bb2c34730988e1e
Alex23888 <99253142+Alex23888@users.noreply.github.com>
2022-10-13 11:59:15 +0300

    Add files via upload

---
+++ calculator/calculator.cpp
@@ -0,0 +1,23 @@
+#include "calculator.h"
+
+int Calculator::Add (double a, double b)
+{
+<<<<<<< HEAD
+<<<<<<< HEAD
+/*
+Яровий Олександр
+Група ІО-06
+2022 рік
+*/
+=======
+	return a + b + 0.5;
+>>>>>>> 4ad40a3 (fix truncation error)
+=======
+	return a + b;
+>>>>>>> cbcb06d (formatting: use tabs instead of spaces)
+}
+
+int Calculator::Sub (double a, double b)
+{
+	return Add (a, -b);
+}
---
+++ calculator/calculator.h
@@ -0,0 +1,17 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator
+{
+<<<<<<< HEAD
+	public:
+		int Add (double, double);
+		int Sub (double, double);
+=======
+    public:
+        static int Add (int, int);
+        static int Mul (double, double);
+>>>>>>> d61d32d (mark member-function as `static`)
+};
+
+#endif//CALCULATOR_H
